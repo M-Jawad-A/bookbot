9103b4e6f2a285c9eb161227de0e53b837a390be
Jawad <c0050298@newcastle.ac.uk>
2024-04-11 21:39:43 +0100

    drew a red line in the main function

--- maze solver/main.py
+++ maze solver/main.py
@@ -2,7 +2,11 @@ from tkinter import Tk, BOTH, Canvas
 
 
 def main():
-    win = Window(800, 600)
+    win = Window(150, 300)
+    point1 = Point(200, 221)
+    point2 = Point(200, 256)
+    line1 = Line(point1, point2, win)
+    win.drawLine("Red", line1)
     win.waitForClose()
 
 
@@ -12,8 +16,8 @@ class Window:
         self.height = height
         self.root = Tk()
         self.root.title("Maze")
-        self.__canvas = Canvas(self.root, bg="White", bd=self.width, height=self.height)
-        self.__canvas.pack(fill=BOTH, expand=1)
+        self.canvas = Canvas(self.root, bg="White", bd=self.width, height=self.height)
+        self.canvas.pack(fill=BOTH, expand=1)
         self.running = False
         self.root.protocol("WM_DELETE_WINDOW", self.close)
 
@@ -33,7 +37,9 @@ class Window:
         self.running = False
 
     def drawLine(self, colour, line):
-        line.Draw(self.__canvas, colour)
+        print("drawing line")
+        drawnLine = Line(line.beginningPoint, line.endPoint, self.canvas)
+        drawnLine.draw(colour)
 
 
 class Point:
@@ -43,14 +49,16 @@ class Point:
 
 
 class Line:
-    def __init__(self, pointA, pointB):
+    def __init__(self, pointA, pointB, window):
         self.beginningPoint = pointA
         self.endPoint = pointB
+        self.window = window
 
-    def draw(self, canvas, colour):
-        canvas.create_line(self.beginningPoint.x, self.beginningPoint.y, self.endPoint.x, self.endPoint.y, fill=colour
-                           , width=2)
-        canvas.pack(fill=BOTH, expand=1)
+    def draw(self, colour):
+        print("doing a draw line")
+        self.window.create_line(self.beginningPoint.x, self.beginningPoint.y, self.endPoint.x, self.endPoint.y, fill=colour,
+                           width=2)
+        self.window.pack(fill=BOTH, expand=1)
 
 
 if __name__ == '__main__':
